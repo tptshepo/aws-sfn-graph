8810dd9a5b99e236cab2343710244936836b91c9
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-01-31 14:19:14 +0200

    Updated

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'aws-sfn-graph'
+import AWSSfnGraph from 'aws-sfn-graph'
 import 'aws-sfn-graph/dist/index.css'
 
+const workflowData =
+  '{\r\n  "Comment": "A Hello World example of the Amazon States Language using Pass states",\r\n  "StartAt": "Hello",\r\n  "States": {\r\n    "Hello": {\r\n      "Type": "Pass",\r\n      "Result": "Hello",\r\n      "Next": "World"\r\n    },\r\n    "World": {\r\n      "Type": "Pass",\r\n      "Result": "World",\r\n      "End": true\r\n    }\r\n  }\r\n}';
+
+  const workflowData2 =
+  '{\r\n  "Comment": "A Hello World example of the Amazon States Language using Pass states",\r\n  "StartAt": "Hello2",\r\n  "States": {\r\n    "Hello2": {\r\n      "Type": "Pass",\r\n      "Result": "Hello",\r\n      "Next": "World"\r\n    },\r\n    "World": {\r\n      "Type": "Pass",\r\n      "Result": "World",\r\n      "End": true\r\n    }\r\n  }\r\n}';
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [data, setData ] = useState(workflowData);
+
+  return (
+    <div>
+      <input type='button' value="Update" onClick={() => { setData(workflowData2)}} />
+      <AWSSfnGraph data={data} width={500} height={500}/>
+    </div>
+
+  ) 
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,145 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class AWSSfnGraph extends React.Component {
+  componentDidMount() {
+    this.containerId = '#svgcontainer'
+    this.graph = null
+    this.hasRenderedOnce = false
+    this.lastStateMachineData = null
+
+    this.renderStateMachine = this.renderStateMachine.bind(this)
+
+    this.handleCenter = this.handleCenter.bind(this)
+    this.handleZoomIn = this.handleZoomIn.bind(this)
+    this.handleZoomOut = this.handleZoomOut.bind(this)
+
+    this.centerBtn = document.getElementById('center')
+    this.zoominBtn = document.getElementById('zoomin')
+    this.zoomoutBtn = document.getElementById('zoomout')
+
+    this.centerBtn.addEventListener('click', this.handleCenter)
+    this.zoominBtn.addEventListener('click', this.handleZoomIn)
+    this.zoomoutBtn.addEventListener('click', this.handleZoomOut)
+
+    if (this.props.data) {
+      this.renderStateMachine(
+        this.props.data,
+        this.props.width,
+        this.props.height
+      )
+    }
+  }
+
+  handleCenter(e) {
+    if (this.lastStateMachineData) {
+      this.renderStateMachine(
+        this.lastStateMachineData,
+        this.props.width,
+        this.props.height
+      )
+    }
+  }
+
+  handleZoomIn(e) {
+    if (this.graph) {
+      this.graph.zoomIn()
+    }
+  }
+
+  handleZoomOut(e) {
+    if (this.graph) {
+      this.graph.zoomOut()
+    }
+  }
+
+  renderStateMachine(data, width = 200, height = 200) {
+    const options = {
+      width,
+      height,
+      resizeHeight: false
+    }
+    this.graph = new globalThis.sfn.StateMachineGraph(
+      JSON.parse(data),
+      this.containerId,
+      options
+    )
+    this.graph.render()
+
+    this.hasRenderedOnce = true
+    this.lastStateMachineData = data
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log('prevProps', prevProps)
+    if (prevProps.data !== this.props.data) {
+      this.renderStateMachine(
+        this.props.data,
+        this.props.width,
+        this.props.height
+      )
+    }
+  }
+
+  componentWillUnmount() {
+    // this.$el.off('change', this.handleChange)
+    // this.$el.chosen('destroy')
+
+    this.centerBtn.removeEventListener('click', this.handleCenter)
+    this.zoominBtn.removeEventListener('click', this.handleZoomIn)
+    this.zoomoutBtn.removeEventListener('click', this.handleZoomOut)
+
+    this.centerBtn = null
+    this.zoominBtn = null
+    this.zoomoutBtn = null
+  }
+
+  handleChange(e) {
+    // this.props.onChange(e.target.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <div id='svgcontainer' className='workflowgraph'>
+          <svg />
+        </div>
+
+        <div className='graph-buttons-container'>
+          <button id='zoomin'>
+            <svg
+              focusable='false'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+            >
+              <line x1='8' y1='1' x2='8' y2='15' />
+              <line x1='15' y1='8' x2='1' y2='8' />
+            </svg>
+          </button>
+          <button id='zoomout'>
+            <svg
+              focusable='false'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+            >
+              <line x1='15' y1='8' x2='1' y2='8' />
+            </svg>
+          </button>
+          <button id='center'>
+            <svg
+              focusable='false'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 16 16'
+            >
+              <circle cx='8' cy='8' r='7' strokeWidth='2' />
+              <circle cx='8' cy='8' r='1' strokeWidth='2' />
+            </svg>
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
+
+AWSSfnGraph.displayName = 'AWSSfnGraph'
+
+export default AWSSfnGraph
