3393085b42d8fe0ae46df7e9dbc132c3a80a0cbe
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-01 05:51:43 +0200

    Refactor component to function component

--- src/index.js
+++ src/index.js
@@ -1,117 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './css/graph-0.1.5.css'
 import './css/state-machine-render.css'
 import './lib/sfn-0.1.5'
 
-class AWSSfnGraph extends React.Component {
-  componentDidMount() {
-    this.containerId = '#svgcontainer'
-    this.graph = null
-    this.hasRenderedOnce = false
-    this.lastStateMachineData = null
+const AWSSfnGraph = (props) => {
+  const { data, width, height } = props
 
-    if (this.props.data) {
-      this.renderStateMachine(
-        this.props.data,
-        this.props.width,
-        this.props.height
-      )
+  const containerId = '#svgcontainer'
+  let graph = null
+
+  useEffect(() => {
+    if (data) {
+      renderStateMachine(data, width, height)
+    }
+  }, [data])
+
+  const handleCenter = (e) => {
+    // if (lastStateMachineData) {
+    renderStateMachine(data, width, height)
+    // }
+  }
+
+  const handleZoomIn = (e) => {
+    if (graph) {
+      graph.zoomIn()
     }
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.data !== this.props.data) {
-      this.renderStateMachine(
-        this.props.data,
-        this.props.width,
-        this.props.height
-      )
+  const handleZoomOut = (e) => {
+    if (graph) {
+      graph.zoomOut()
     }
   }
 
-  componentWillUnmount() {}
-
-  handleCenter = (e) => {
-    if (this.lastStateMachineData) {
-      this.renderStateMachine(
-        this.lastStateMachineData,
-        this.props.width,
-        this.props.height
-      )
-    }
-  }
-
-  handleZoomIn = (e) => {
-    if (this.graph) {
-      this.graph.zoomIn()
-    }
-  }
-
-  handleZoomOut = (e) => {
-    if (this.graph) {
-      this.graph.zoomOut()
-    }
-  }
-
-  renderStateMachine = (data, width = 200, height = 200) => {
+  const renderStateMachine = (data, width = 200, height = 200) => {
     const options = {
       width,
       height,
       resizeHeight: false
     }
     const json = typeof data === 'string' ? JSON.parse(data) : data
-    this.graph = new globalThis.sfn.StateMachineGraph(
-      json,
-      this.containerId,
-      options
-    )
-    this.graph.render()
-
-    this.hasRenderedOnce = true
-    this.lastStateMachineData = data
+    graph = new globalThis.sfn.StateMachineGraph(json, containerId, options)
+    graph.render()
   }
 
-  render() {
-    return (
-      <div className='workflowgraph'>
-        <div id='svgcontainer'>
-          <svg />
-        </div>
-
-        <div className='graph-buttons-container'>
-          <button onClick={this.handleZoomIn}>
-            <svg
-              focusable='false'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 16 16'
-            >
-              <line x1='8' y1='1' x2='8' y2='15' />
-              <line x1='15' y1='8' x2='1' y2='8' />
-            </svg>
-          </button>
-          <button onClick={this.handleZoomOut}>
-            <svg
-              focusable='false'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 16 16'
-            >
-              <line x1='15' y1='8' x2='1' y2='8' />
-            </svg>
-          </button>
-          <button onClick={this.handleCenter}>
-            <svg
-              focusable='false'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 16 16'
-            >
-              <circle cx='8' cy='8' r='7' strokeWidth='2' />
-              <circle cx='8' cy='8' r='1' strokeWidth='2' />
-            </svg>
-          </button>
-        </div>
+  return (
+    <div className='workflowgraph'>
+      <div id='svgcontainer'>
+        <svg />
       </div>
-    )
-  }
+
+      <div className='graph-buttons-container'>
+        <button onClick={handleZoomIn}>
+          <svg
+            focusable='false'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+          >
+            <line x1='8' y1='1' x2='8' y2='15' />
+            <line x1='15' y1='8' x2='1' y2='8' />
+          </svg>
+        </button>
+        <button onClick={handleZoomOut}>
+          <svg
+            focusable='false'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+          >
+            <line x1='15' y1='8' x2='1' y2='8' />
+          </svg>
+        </button>
+        <button onClick={handleCenter}>
+          <svg
+            focusable='false'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+          >
+            <circle cx='8' cy='8' r='7' strokeWidth='2' />
+            <circle cx='8' cy='8' r='1' strokeWidth='2' />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 AWSSfnGraph.displayName = 'AWSSfnGraph'
