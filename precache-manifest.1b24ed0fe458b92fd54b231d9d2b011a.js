e74fe072663398e6a6ddd50572d7fbbaa4799d37
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-01 08:33:49 +0200

    Updates

---
+++ precache-manifest.1b24ed0fe458b92fd54b231d9d2b011a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f68b89988fafaee215e038f1ae2a0ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea6df3f2c85c00d740bd",
+    "url": "./static/css/main.6f02d724.chunk.css"
+  },
+  {
+    "revision": "0cc73c7ae9f6bbc93782",
+    "url": "./static/js/2.00ca3978.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.00ca3978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6df3f2c85c00d740bd",
+    "url": "./static/js/main.da221f4f.chunk.js"
+  },
+  {
+    "revision": "00d805bba8817db6cdc5",
+    "url": "./static/js/runtime-main.2c8c916f.js"
+  }
+]);
